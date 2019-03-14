261321aceea3bc9202a86313c7c4a1710ebdc561
TheAllenAnker <theallenanker@gmail.com>
2019-03-14 16:58:34 +0800

    Added feature: avatar uploading.

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,12 +1,41 @@
-// pages/Mypage/mypage.js
 const qiniuUploader = require("../../utils/qiniuUploader");
 const app = getApp();
 var lastPage = null;
+var serverUrl = app.globalData.serverUrl;
+
+function initQiniu() {
+  var options = {
+    region: 'SCN', // 华北区
+    uptokenURL: serverUrl + '/user/upToken',
+    // uptoken: 'xxxx=',
+    domain: 'http://pnu0mosmq.bkt.clouddn.com',
+    shouldUseQiniuFileName: false
+  };
+  qiniuUploader.init(options);
+}
 
 Page({
   /**
    * 页面的初始数据
    */
+  data: {
+    imageObject: {}
+  },
+  //事件处理函数
+  onLoad: function() {
+    console.log('onLoad')
+    var that = this;
+  },
+
+  changeAvatar: function() {
+    var that = this;
+    didPressChooesImage(that);
+  },
+
+  didCancelTask: function() {
+    this.data.cancelTask()
+  },
+
   data: {
     avatar: null,
     serverUrl: app.globalData.serverUrl,
@@ -72,21 +101,6 @@ Page({
     });
   },
 
-  initQiniu: function () {
-    var options = {
-      region: 'SCN', // 华北区
-      uptokenURL: 'http://localhost:8080/iMall/qiniu/uptoken',
-      // uptoken: 'xxxx=',
-      domain: 'http://pnu0mosmq.bkt.clouddn.com',
-      shouldUseQiniuFileName: false
-    };
-    qiniuUploader.init(options);
-  },
-
-  changeAvatar: function() {
-    
-  },
-
   onLoad: function(options) {
     this.setData({
       userInfo: app.globalData.userInfo
@@ -97,9 +111,9 @@ Page({
     var that = this;
     var userInfo = app.globalData.userInfo;
     console.log(userInfo);
-    if(userInfo != null) {
+    if (userInfo != null) {
       that.setData({
-        avatar: that.data.serverUrl + userInfo.avatar
+        avatar: userInfo.avatar
       });
     } else {
       that.setData({
@@ -108,3 +122,61 @@ Page({
     }
   }
 })
+
+function didPressChooesImage(that) {
+  initQiniu();
+  // 微信 API 选文件
+  wx.chooseImage({
+    count: 1,
+    success: function(res) {
+      var filePath = res.tempFilePaths[0];
+      // 交给七牛上传
+      wx.showLoading({
+        title: '请等待...',
+      })
+      qiniuUploader.upload(filePath, (res) => {
+          that.setData({
+            'imageObject': res
+          });
+          var fileUrl = res.fileUrl;
+          console.log('file url is: ' + fileUrl)
+          wx.request({
+            url: serverUrl + '/user/updateUserInfo',
+            method: "POST",
+            data: {
+              avatar: fileUrl,
+              id: app.globalData.userInfo.id
+            },
+            header: {
+              'content-type': 'application/json'
+            },
+            success: function(res) {
+              if (res.data.status == 200) {
+                that.onLoad;
+              } else {
+                // 失败弹出框
+                wx.showToast({
+                  title: "上传失败",
+                  icon: 'none',
+                  duration: 3000
+                })
+              }
+            }
+          });
+          wx.hideLoading();
+        }, (error) => {
+          console.error('error: ' + JSON.stringify(error));
+        },
+        null,
+        (progress) => {
+          console.log('上传进度', progress.progress)
+          console.log('已经上传的数据长度', progress.totalBytesSent)
+          console.log('预期需要上传的数据总长度', progress.totalBytesExpectedToSend)
+        }, cancelTask => that.setData({
+          cancelTask
+        })
+      );
+    }
+  })
+  wx.hideLoading();
+}
